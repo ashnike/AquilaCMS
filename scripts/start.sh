9ffe556686774b8fdff6cc68721e3fbb32b4ee9f
Ubuntu <ubuntu@ip-10-0-135-86.ec2.internal>
2023-10-15 02:45:10 +0000

    new scritps

---
+++ scripts/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Navigate to your application
+cd /home/ubuntu/AquilaCMS
+# Define the Docker image name
+image_name="auxila"
+
+# Check if a container with the same name exists
+if [ "$(sudo docker ps -aq -f name=$image_name)" ]; then
+    # Stop and remove the existing container
+    sudo docker stop $image_name
+    sudo docker rm $image_name
+fi
+
+# Run the Docker container
+sudo docker run -d -p 3010:3010 --name auxila --restart always auxila:latest
+
